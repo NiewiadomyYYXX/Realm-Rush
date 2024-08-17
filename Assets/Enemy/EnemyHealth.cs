e4a0dec6c8ed0a682d2a4dad9a9e7cb9695e06d4
NiewiadomyYYXX <niewiadomyyyxx49@gmail.com>
2024-08-17 12:04:16 +0200

    Making Turret work

---
+++ Assets/Enemy/EnemyHealth.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] int maxHP = 5;
+    int HP = 0;
+
+    void Start()
+    {
+        HP = maxHP;
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+    }
+
+    private void ProcessHit()
+    {
+        HP--;
+        if (HP <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
