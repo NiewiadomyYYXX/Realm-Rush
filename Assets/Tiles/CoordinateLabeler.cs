b02c0d2b24c0d058fdd669f1b9eb4d281c75cdd2
NiewiadomyYYXX <niewiadomyyyxx49@gmail.com>
2024-08-20 15:52:20 +0200

    pathfinding part 2

--- Assets/Tiles/CoordinateLabeler.cs
+++ Assets/Tiles/CoordinateLabeler.cs
@@ -10,16 +10,19 @@ public class CoordinateLabeler : MonoBehaviour
 {
     [SerializeField] Color defaultColor = Color.white;
     [SerializeField] Color blockColor = Color.gray;
+    [SerializeField] Color exploredColor = Color.yellow;
+    [SerializeField] Color pathColor = new Color(1f, .5f, 0f);
 
 
     TMP_Text label;
     Vector2Int coordinates = new Vector2Int();
-    Waypoint waypoint;
+    GridManager grid;
 
     void Awake()
     {
         label = GetComponent<TextMeshPro>();
-        waypoint = GetComponentInParent<Waypoint>();
+        label.enabled = false;
+        grid = FindObjectOfType<GridManager>();
         DisplayCoordinates();
     }
     void Update()
@@ -28,6 +31,7 @@ public class CoordinateLabeler : MonoBehaviour
        {
             DisplayCoordinates();
             UpdateObjectName();
+            label.enabled = true;
        }
 
         SetLabelColor();
@@ -49,13 +53,27 @@ public class CoordinateLabeler : MonoBehaviour
 
     void SetLabelColor()
     {
-        if(waypoint.IsPlaceable)
-        {
-            label.color = defaultColor;
-        } 
-        else
+        if(grid == null) { return; }
+
+        Node node = grid.GetNode(coordinates);
+
+        if(node == null ) { return; }
+
+        if(!node.isWalkable)
         {
             label.color = blockColor;
+        }
+        else if(node.isPath)
+        {
+            label.color = pathColor;
+        }
+        else if (node.isExplored)
+        {
+            label.color = exploredColor;
+        }
+        else
+        {
+            label.color = defaultColor;
         }
 
     }
